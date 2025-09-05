06ec4061d19e8a0b5ba05894c70b3e16f619bf67
ErwannRousseau <erwann.rousseau@icloud.com>
2025-09-05 18:39:00 +0200

    update lazyvim config

---
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        swift = { "swiftformat" },
+      },
+      formatters = {
+        swiftformat = {
+          prepend_args = { "--indent", "2" },
+        },
+      },
+    },
+  },
+}
--- dot_config/nvim/lua/plugins/trouble.lua
+++ dot_config/nvim/lua/plugins/trouble.lua
@@ -7,5 +7,34 @@ return {
       "<cmd>Trouble diagnostics toggle filter.buf=0 focus=true<cr>",
       desc = "Trouble: Buffer Diagnostics",
     },
+    {
+      "<leader>xd",
+      function()
+        local enabled = vim.diagnostic.is_enabled()
+        if enabled then
+          vim.diagnostic.enable(false)
+          vim.notify("Diagnostics disabled", vim.log.levels.WARN)
+        else
+          vim.diagnostic.enable(true)
+          vim.notify("Diagnostics enabled", vim.log.levels.INFO)
+        end
+      end,
+      desc = "Toggle diagnostics (global)",
+    },
+    {
+      "<leader>xD",
+      function()
+        local bufnr = vim.api.nvim_get_current_buf()
+        local enabled = vim.diagnostic.is_enabled({ bufnr = bufnr })
+        if enabled then
+          vim.diagnostic.enable(false, { bufnr = bufnr })
+          vim.notify("Diagnostics disabled (buffer)", vim.log.levels.WARN)
+        else
+          vim.diagnostic.enable(true, { bufnr = bufnr })
+          vim.notify("Diagnostics enabled (buffer)", vim.log.levels.INFO)
+        end
+      end,
+      desc = "Toggle diagnostics (buffer)",
+    },
   },
 }
