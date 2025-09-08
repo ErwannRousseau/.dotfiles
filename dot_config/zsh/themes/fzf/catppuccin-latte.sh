f8642f274b30a9d82a2e2d13d20a976c923eed26
ErwannRousseau <erwann.rousseau@icloud.com>
2025-09-08 09:35:01 +0200

    add custom theme for fzf

---
+++ dot_config/zsh/themes/fzf/catppuccin-latte.sh
@@ -0,0 +1,18 @@
+export FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} \
+  --ansi --layout=reverse --height=100% --border=none --info=inline-right --highlight-line \
+  --color=bg:#eff1f5 \
+  --color=bg+:#d8dae1 \
+  --color=gutter:#eff1f5 \
+  --color=fg:#4c4f69 \
+  --color=hl:#1e66f5 \
+  --color=hl+:#1e66f5 \
+  --color=prompt:#1e66f5 \
+  --color=query:#4c4f69:regular \
+  --color=pointer:#ea76cb \
+  --color=marker:#ea76cb \
+  --color=spinner:#d20f39 \
+  --color=header:#df8e1d \
+  --color=separator:#df8e1d \
+  --color=border:#bcc0cc \
+  --color=scrollbar:#bcc0cc \
+"
