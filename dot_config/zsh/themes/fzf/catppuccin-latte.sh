4a5070574c93c188f7f68d96591d01f2e2fefc53
ErwannRousseau <erwann.rousseau@icloud.com>
2025-09-14 15:37:22 +0200

    update zsh theme and fzf theme

--- dot_config/zsh/themes/fzf/catppuccin-latte.sh
+++ dot_config/zsh/themes/fzf/catppuccin-latte.sh
@@ -1,18 +1,8 @@
 export FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} \
   --ansi --layout=reverse --height=100% --border=none --info=inline-right --highlight-line \
-  --color=bg:#eff1f5 \
-  --color=bg+:#d8dae1 \
-  --color=gutter:#eff1f5 \
-  --color=fg:#4c4f69 \
-  --color=hl:#1e66f5 \
-  --color=hl+:#1e66f5 \
-  --color=prompt:#1e66f5 \
-  --color=query:#4c4f69:regular \
-  --color=pointer:#ea76cb \
-  --color=marker:#ea76cb \
-  --color=spinner:#d20f39 \
-  --color=header:#df8e1d \
-  --color=separator:#df8e1d \
-  --color=border:#bcc0cc \
-  --color=scrollbar:#bcc0cc \
+  --color=bg+:#CCD0DA,bg:#EFF1F5,spinner:#DC8A78,hl:#D20F39 \
+  --color=fg:#4C4F69,header:#D20F39,info:#8839EF,pointer:#DC8A78 \
+  --color=marker:#7287FD,fg+:#4C4F69,prompt:#8839EF,hl+:#D20F39 \
+  --color=selected-bg:#BCC0CC \
+  --color=border:#9CA0B0,label:#4C4F69 \
 "
