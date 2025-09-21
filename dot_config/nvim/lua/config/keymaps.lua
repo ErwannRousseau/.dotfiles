58826172af21edfa39552adc47cff5cb4a101ec0
ErwannRousseau <erwann.rousseau@icloud.com>
2025-09-21 22:44:52 +0200

    update nvim config for typescript

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -3,7 +3,13 @@
 -- Add any additional keymaps here
 local map = vim.keymap.set
 
-local escape_keys = { "jj", "hh", "kk", "ll" }
+local escape_keys = { "jj", "hh", "kk" }
 for _, key in ipairs(escape_keys) do
   map("i", key, "<Esc>", { desc = "Exit insert mode" })
 end
+
+map({ "n", "x", "i" }, "<C-q>", function()
+  Snacks.bufdelete()
+end, { desc = "Delete Buffer" })
+
+map("n", "U", "<C-r>", { desc = "Redo" })
--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -2,8 +2,12 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.g.lazyvim_picker = "fzf"
+
 -- Set to false to disable auto format
 vim.g.lazyvim_eslint_auto_format = true
 
 -- Enable this option to avoid conflicts with Prettier.
 vim.g.lazyvim_prettier_needs_config = true
+
+vim.g.root_spec = { "cwd" }
