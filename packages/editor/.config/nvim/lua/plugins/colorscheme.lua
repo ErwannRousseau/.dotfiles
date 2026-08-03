2fea9d56a8ffc5fef6bdfd5abf810a8ca828d8d7
ErwannRousseau <erwann.rousseau@icloud.com>
2026-08-03 15:06:03 -0400

    refactor: migrate dotfiles to stow

---
+++ packages/editor/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  { -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require("catppuccin").setup({ flavour = "latte", auto_integrations = true })
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
